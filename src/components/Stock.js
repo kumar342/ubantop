6c0a85c2eedb78cc2215ca714b947d2832e94e7a
Hemanth Kumar <56812697+kumar342@users.noreply.github.com>
2020-07-27 11:28:11 +0530

    initail commit

---
+++ src/components/Stock.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Stock extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-9">$$$$$$$$$$$$$$$$$</div>
+        <div className="col-md-3">&&&&&&&&&&&&&&&&&</div>
+      </div>
+    );
+  }
+}
